074299424555212ef341fafffc1f9957e274c6cf
carlos.arturo.farias@ibm.com <carlos-arturo-farias@ibm.com>
2023-11-23 15:39:46 -0600

    - layout

--- src/app/_metronic/layout/core/page-info.service.ts
+++ src/app/_metronic/layout/core/page-info.service.ts
@@ -18,7 +18,7 @@ export class PageInfo {
 })
 export class PageInfoService {
   public title: BehaviorSubject<string> = new BehaviorSubject<string>(
-    'Dashboard'
+    ''
   );
   public description: BehaviorSubject<string> = new BehaviorSubject<string>('');
   public breadcrumbs: BehaviorSubject<Array<PageLink>> = new BehaviorSubject<
